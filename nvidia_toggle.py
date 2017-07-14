b02fcb6c9d799ac947eafa896d1d29efd02aae67
Jake Magers <jmagers1@mail.csuchico.edu>
2017-07-14 04:15:10 -0700

    Finish parsing monitor data

--- nvidia_toggle.py
+++ nvidia_toggle.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3.5
 
+import os
 import re
+import subprocess
 import sys
 
 CONFIG_PATH = '/etc/X11/xorg.conf'
+MONITORS_XML = os.path.join(os.environ['HOME'], '.config', 'monitors.xml')
+
+
+class Monitor:
+    def __init__(self, info):
+        self.info = info.strip()
+        self.name = info.split(':')[0].strip()
+        self.pos = int(info.split('+')[1])
+        self.width = None
+        self.is_connected = None
+
 
 # Parse config file for monitor info
-monitors = {}
+monitors = []
 try:
     with open(CONFIG_PATH, 'r') as conf:
         for line in conf:
@@ -16,13 +29,23 @@ try:
             except AttributeError:
                 continue
             for monitor_string in settings_string.split(','):
-                monitor_elements = monitor_string.split(':')
-                mon_id, mon_setting = [x.strip() for x in monitor_elements]
-                monitors[mon_id] = mon_setting
+                monitors.append(Monitor(monitor_string))
         if not monitors:
             sys.exit("No monitors found.")
 except FileNotFoundError:
     sys.exit("'%s' does not exist" % CONFIG_PATH)
 
-# Get set of connected monitors
-connected_monitors = set()
+# Find out which monitors are currently connected
+p = subprocess.run(['xrandr', '-q'],
+                   stdout=subprocess.PIPE,
+                   universal_newlines=True)
+for monitor in monitors:
+    query = monitor.name + r' connected (primary )?\d+x\d+\+\d+\+\d+'
+    monitor.is_connected = bool(re.search(query, p.stdout))
+
+# Get width of each monitor
+with open(MONITORS_XML, 'r') as xml:
+    text = xml.read()
+    for monitor in monitors:
+        query = r'name="' + monitor.name + r'"[.\s\S]*?<width>(\d+)</width>'
+        monitor.width = int(re.search(query, text).group(1))
